5ea05d6151e2dd07b50abfb0c9689a556bfb1ad0
Gerardo Cardoso <ger9047@gmail.com>
2025-07-30 23:47:50 -0600

    Configurando el RunCucumber

---
+++ src/test/java/configs/RunCucumber.java
@@ -0,0 +1,31 @@
+package configs;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "definitions",
+        tags = "@testAserciones",
+        publish = true
+)
+
+public class RunCucumber {
+
+    public RunCucumber(){
+    }
+
+@AfterClass
+    public static void quitDriver(){
+        System.out.println("Se cierra el web driver");
+
+
+}
+
+
+
+}
